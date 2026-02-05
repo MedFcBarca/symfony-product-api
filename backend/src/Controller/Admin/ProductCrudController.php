f48b0333cceeaa572e92cb4bec617fa7640b5d47
Abbad Mohamed <abbadmohamed@MacBook-Pro-de-Abbad.local>
2026-02-05 15:21:28 +0100

    Add EasyAdmin dashboard and CRUD for Product and Category

---
+++ backend/src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('designation'),
+            AssociationField::new('categories')
+                ->setFormTypeOption('by_reference', false)
+                ->setRequired(false),
+        ];
+    }
+}
